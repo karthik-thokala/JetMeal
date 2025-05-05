954570863482c202d9a649f7d218a05914950ec0
Karthik tinku <karthiktinku625@email.com>
2025-05-05 20:36:46 +0530

    Fix: include all Parcel dependencies for Netlify

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+    
+    require("dotenv").config();
+  }
+  
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header.js";
